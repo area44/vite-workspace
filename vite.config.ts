c182f44aee74364d6259b749e96443ab5d4a676f
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-10-24 14:54:18 +0700

    bump actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const base = process.env.base || "/";
+const base = process.env.BASE || "/";
 
 // https://vite.dev/config/
 export default defineConfig({
